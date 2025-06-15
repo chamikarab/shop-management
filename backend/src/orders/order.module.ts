4dd582f5e9462e3eb62b01b2dcb02341d0823559
chamikarabandara <chamikara38@gmail.com>
2025-06-15 15:55:43 +0530

    feat : order function implemented

---
+++ backend/src/orders/order.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { OrderController } from './order.controller';
+import { OrderService } from './order.service';
+import { Order, OrderSchema } from './order.schema';
+import { Product, ProductSchema } from '../product/product.schema';
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: Order.name, schema: OrderSchema },
+      { name: Product.name, schema: ProductSchema },
+    ]),
+  ],
+  controllers: [OrderController],
+  providers: [OrderService],
+  exports: [OrderService], // Optional: Export if needed elsewhere
+})
+export class OrderModule {}
